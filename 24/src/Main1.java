d7cf1c2b3e51db3649ded5d40c02958b873c76ef
obscurecore <43168898+obscurecore@users.noreply.github.com>
2019-04-23 23:12:12 +0300

    Add files via upload

--- 24/src/Main1.java
+++ 24/src/Main1.java
@@ -10,33 +10,24 @@ public  class  Main1{
         List<Pup> lis = new ArrayList<>();
         lis.add(a1);
         lis.add(a2);
-/*
+
         Collections.sort(lis, new Comparator<Pup>(){
             public int compare(Pup p1, Pup p2){
                     int sa = (int)p1.getID();
                     int sb = (int)p2.getID();
                     int v = Integer.compare(sa,sb);
                     return v;
-                }
-        });
-*/
+                }});
+
         Collections.sort(lis, (o1, o2) -> o1.getID() - o2.getID());
 
         for (int i = 0; i <lis.size() ; i++) {
             System.out.println(lis.get(i).getID());
         }
-/*
+
         Comparator<Pup> c = new ByNumCOmp();
-        Comparator<Pup> id =
-                (o1, o2) -> o1.getID() - o2.getID();
-        /*
-        PlayList playList = new PlayList(10, c);
-        playList.add(a1);
-        playList.add(a2);
-        playList.add(a3);
-        playList.add(a4);
-        playList.show();
-        getDuration() - o2.getDuration();
-        */
+        Comparator<Pup> id = (o1, o2) -> o1.getID() - o2.getID();
+
+
     }
 }
